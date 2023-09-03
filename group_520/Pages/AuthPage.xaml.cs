bd3cb601c14c8a3badb4f285c49a7aa21d62d42a
sevase <kotaseva8@gmail.com>
2023-09-03 19:32:01 +0300

    сделана авторизация

---
+++ group_520/Pages/AuthPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace group_520.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            string textInTxtBox = PassworBox.Text.ToLower();
+            // Очистите текст, когда TextBox получает фокус
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text.ToLower() == textInTxtBox)
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string textInTxtBox = PassworBox.Text.ToLower();
+            // Восстановите текст, если TextBox пустой
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textInTxtBox;
+            }
+        }
+    }
+}
